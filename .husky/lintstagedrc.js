642baa29a52f2e83356e2d848e8a5013baa01906
程辉 <1354332468@qq.com>
2022-09-20 14:55:33 +0800

    ci: pre-commit

--- .husky/lintstagedrc.js
+++ .husky/lintstagedrc.js
@@ -1,6 +1,3 @@
 module.exports = {
     'src/**/*.{vue,js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
-    '{!(package)*.json,.!(browserslist)*rc}': ['prettier --write--parser json'],
-    'package.json': ['prettier --write'],
-    '*.md': ['prettier --write'],
 }
