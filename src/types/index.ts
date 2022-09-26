54dcd9e8e4cc9e67df14b8db7e5999719e6bd10b
程辉 <1354332468@qq.com>
2022-09-26 17:42:46 +0800

    feat: add types

---
+++ src/types/index.ts
@@ -0,0 +1,37 @@
+import type { VNode, PropType } from 'vue'
+import type { VueTypeValidableDef, VueTypeDef } from 'vue-types'
+
+declare type VNodeChildAtom = VNode | string | number | boolean | null | undefined | void
+
+export type VueNode = VNodeChildAtom | VNodeChildAtom[] | JSX.Element
+
+const initDefaultProps = <T>(
+    types: T,
+    defaultProps: {
+        [K in keyof T]?: T[K] extends VueTypeValidableDef<infer U>
+            ? U
+            : T[K] extends VueTypeDef<infer U>
+            ? U
+            : T[K] extends { type: PropType<infer U> }
+            ? U
+            : any
+    },
+): T => {
+    const propTypes: T = { ...types }
+    Object.keys(defaultProps).forEach(k => {
+        const prop = propTypes[k] as VueTypeValidableDef
+        if (prop) {
+            if (prop.type || prop.default) {
+                prop.default = defaultProps[k]
+            } else if (prop.def) {
+                prop.def(defaultProps[k])
+            } else {
+                propTypes[k] = { type: prop, default: defaultProps[k] }
+            }
+        } else {
+            throw new Error(`not have ${k} prop`)
+        }
+    })
+    return propTypes
+}
+export default initDefaultProps
