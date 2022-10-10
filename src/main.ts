b7875c39a59e1eeedefb036a20ce041ad630e176
程辉 <1354332468@qq.com>
2022-10-10 11:07:22 +0800

    feat: 全局异常日志和ts .vue文件声明

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,11 @@ import 'uno.css'
 
 const app = createApp(App)
 
+// 全局异常处理器
+app.config.errorHandler = (err, vm, info) => {
+    console.log('[全局异常]', err, vm, info)
+}
+
 app.use(createPinia())
 app.use(router)
 
