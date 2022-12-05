e0af41a02b4b47cb5738f293c2178016aaaf8386
程辉 <1354332468@qq.com>
2022-12-05 15:05:42 +0800

    chore: single-spa

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@
         <RouterLink to="/about">About</RouterLink>
     </div>
     <RouterView />
+    <!-- 定义子应用容器 -->
+    <div id="child-vue-app"></div>
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { createApp } from 'vue'
+import { createApp, h } from 'vue'
+import { registerApplication, start } from 'single-spa'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
@@ -7,7 +8,53 @@ import router from './router'
 import './assets/main.css'
 import 'uno.css'
 
-const app = createApp(App)
+/**
+ * 子应用脚本
+ *
+ * @param { string } url 子应用脚本文件地址
+ * @returns Promise
+ */
+function runScript(url) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script')
+        script.src = url
+        script.onload = resolve
+        script.onerror = reject
+        const firstScript = document.getElementsByTagName('script')[0]
+        firstScript.parentElement.insertBefore(script, firstScript)
+    })
+}
+
+/**
+ * 加载子应用
+ *
+ * @param { string } url 子应用地址
+ * @param { string } appName 子应用名，全局唯一
+ * @returns Promise
+ */
+function loadApp(url, appName) {
+    return async () => {
+        await runScript(url + '/js/app.js')
+        return window[appName]
+    }
+}
+
+const app1 = {
+    name: 'app1',
+    app: loadApp('http://localhost:3000', 'app1'),
+    activeWhen: location => location.pathname.startsWith('/app1'),
+    customProps: {},
+}
+
+const app = createApp({
+    created() {
+        registerApplication(app1)
+    },
+    mounted() {
+        start()
+    },
+    render: () => h(App),
+})
 
 // 全局异常处理器
 app.config.errorHandler = (err, vm, info) => {
