7171cd93ecfd4cfa606c5ab751f30c2817111469
程辉 <1354332468@qq.com>
2022-12-07 18:53:56 +0800

    chore: qiankun

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp, h } from 'vue'
-import singleSpaVue from 'single-spa-vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
@@ -7,22 +6,13 @@ import router from './router'
 
 import './assets/main.css'
 import 'uno.css'
-import { isWindow } from '@vueuse/core'
 
-const options: {
-    el?: string
-    render: () => {}
-} = {
-    el: '#child-vue-app',
-    render: () => h(App),
-}
+let appDom: any = null
 
-// 支持应用独立运行和部署
-if (!window.singleSpaNavigate) {
-    delete options.el
+const isQiankun = window.__POWERED_BY_QIANKUN__
 
+function render() {
     const app = createApp(App)
-
     // 全局异常处理器
     app.config.errorHandler = (err, vm, info) => {
         console.log('[全局异常]', err, vm, info)
@@ -32,19 +22,44 @@ if (!window.singleSpaNavigate) {
     app.use(router)
 
     app.mount('#app')
+    appDom = app
 }
 
-const vueLifecycles = singleSpaVue({
-    createApp,
-    appOptions: options,
-    handleInstance(app) {
-        app.use(createPinia())
-        app.use(router)
-    },
-})
+/**
+ * bootstrap 只会在微应用初始化的时候调用一次，下次微应用重新进入时会直接调用 mount 钩子，不会再重复触发 bootstrap。
+ * 通常我们可以在这里做一些全局变量的初始化，比如不会在 unmount 阶段被销毁的应用级别的缓存等。
+ */
+export async function bootstrap() {
+    console.log('react app bootstraped')
+}
 
-export const bootstrap = vueLifecycles.bootstrap // 启动时
-export const mount = vueLifecycles.mount // 挂载时
-export const unmount = vueLifecycles.unmount // 卸载时
+/**
+ * 应用每次进入都会调用 mount 方法，通常我们在这里触发应用的渲染方法
+ */
+export async function mount(props: any) {
+    console.log('让我们来看看mount里面有啥', props)
+    if (!appDom) {
+        render()
+    }
+    console.log('此时的appdom是什么呢', appDom)
+    appDom.mount(
+        props.container ? props.container.querySelector('#child-site_app') : '#child-site_app',
+    )
+}
 
-export default vueLifecycles
+if (!isQiankun) {
+    render()
+    appDom.mount('#child-app')
+}
+
+/**
+ * 应用每次 切出/卸载 会调用的方法，通常在这里我们会卸载微应用的应用实例
+ */
+export async function unmount(props: any) {
+    console.log('让我们来看看unmount里面有啥', props)
+    if (!appDom) {
+        render()
+    }
+    appDom.unmount()
+    appDom = null
+}
